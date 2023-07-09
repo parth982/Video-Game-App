dd1f558598b50a391eb9a1ad612193d5c0acce3d
Parth_Ghatge <parthghatge982@gmail.com>
2023-07-09 14:47:16 +0530

    Handle Games with No Image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from '../assets/no-image.webp'
+
 const getOptimisedImage = (url: string) => {
-    if (!url) return;
+    if (!url) return noImage;
     return url.replace('media/', 'media/crop/600/400/');
 }
 
